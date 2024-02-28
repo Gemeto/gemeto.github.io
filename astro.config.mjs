e581c7e254e18fa3ce18f8f49844fa7de855599c
Gemeto <skaitdan@gmail.com>
2024-02-28 04:25:48 +0100

    Paths changed for git actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://gemeto.github.io',
-  base: '/JSONAstroPorfolio',
+  base: '/',
   integrations: [tailwind(), react()]
 });
