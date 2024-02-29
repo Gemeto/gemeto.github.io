902615ac4e5d798ade4228bcbe24cb246f858ea2
Gemeto <skaitdan@gmail.com>
2024-02-29 15:56:23 +0100

    Lamp responsive fix

--- src/components/ui/lamp.tsx
+++ src/components/ui/lamp.tsx
@@ -1,14 +1,26 @@
 "use client";
-import React from "react";
-import { motion } from "framer-motion";
+import { motion, animate } from "framer-motion";
 import { cn } from "../../utils/cn";
+import { useEffect, useState } from "react";
 
 export const LampContainer = ({
   className,
 }: {
   className?: string;
 }) => {
-  const isMobile = window.innerWidth < 700;
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 700);
+  window.addEventListener('resize', function(){
+    setIsMobile(window.innerWidth < 700);
+  });
+  useEffect(() => {
+    if (isMobile) {
+      animate("#lamp", {width: "6rem"}, {duration: 0.8});
+      animate("#light", { width: "10rem" }, { duration: 0.8 });
+    }else {
+      animate("#lamp", { width: "16rem" }, { duration: 0.8 });
+      animate("#light", { width: "30rem" }, { duration: 0.8 });
+    }
+  }, [isMobile]);
   return (
     <div
       className={cn(
@@ -18,6 +30,7 @@ export const LampContainer = ({
     >
       <div className="relative flex w-full flex-1 items-center justify-center isolate z-0 mt-40">
         <motion.div
+          id="lamp"
           initial={{ width: isMobile ? "3rem" : "8rem" }}
           whileInView={{ width: isMobile ? "6rem" : "16rem" }}
           transition={{
@@ -29,6 +42,7 @@ export const LampContainer = ({
         ></motion.div>
         <div className="absolute inset-auto z-50 h-36 -translate-y-1/2 rounded-full bg-cyan-500 opacity-50 blur-3xl"></div>
         <motion.div
+          id="light"
           initial={{ width: isMobile ? "5rem" : "15rem" }}
           whileInView={{ width: isMobile ? "10rem" : "30rem" }}
           transition={{
