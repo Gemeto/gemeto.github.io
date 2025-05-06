3b596d73f3db107333429c2b2eb5852f446fa9ae
Daniel Martinez Valeriano <32536834+Gemeto@users.noreply.github.com>
2025-05-06 22:47:15 +0000

    Style changes

--- src/components/ui/boxes-background.tsx
+++ src/components/ui/boxes-background.tsx
@@ -68,22 +68,6 @@ export const BoxesCore = ({ className, initialRows = 10, initialCols = 10, ...re
               key={`col` + j}
               className="w-16 h-8 border-r border-t border-slate-700 relative paint-box"
             >
-              {j % 2 === 0 && i % 2 === 0 ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="absolute h-6 w-10 -top-[14px] -left-[22px] text-slate-700 stroke-[1px] pointer-events-none"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 6v12m6-6H6"
-                  />
-                </svg>
-              ) : null}
             </motion.div>
           ))}
         </motion.div>
